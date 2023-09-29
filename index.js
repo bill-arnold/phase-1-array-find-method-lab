2364712482782c4369b8d35e028ce521f2e61dec
bill <bill.arnold@student.moringaschool.com>
2023-09-29 23:42:01 +0300

    Your commit message here

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+    //...
+  ];
+  
+  function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+  
+    // Check if a winning game was found
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  //  usage:
+  const winYear = superbowlWin(record);
+  console.log(winYear); // This will output the year of the win or undefined if no win is found
+  
